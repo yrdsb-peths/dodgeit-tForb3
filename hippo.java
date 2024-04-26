240fea408ce8907b34d2da71f2a9392db70e01c3
tForb3 <349950253@gapps.yrdsb.ca>
2024-04-25 22:51:09 -0400

    added hip1

---
+++ hippo.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class hippo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class hippo extends Actor
+{
+    /**
+     * Act - do whatever the hippo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        hippo hip1 = new hippo();
+        addedToWorld(hip1, 100, 100);// Add your action code here.
+    }
+}
